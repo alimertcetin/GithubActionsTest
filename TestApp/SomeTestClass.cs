b2b95a9f734eddd0b74ca4ec3f2ed8d14ea6517d
alimertcetin <alimertcetin@users.noreply.github.com>
2023-10-30 17:49:46 +0000

    deploy: 7594d5616e7ef5cf8c8e1edc87dad5093279d5a2

---
+++ TestApp/SomeTestClass.cs
@@ -0,0 +1,39 @@
+﻿namespace TestApp
+{
+    public class SomeTestClass
+    {
+        public const string TEST_STRİNG = "Has some test value";
+
+        /// <summary>
+        /// Factory method to create <see cref="SomeTestClass"/>
+        /// </summary>
+        /// <returns>An usable <see cref="SomeTestClass"/></returns>
+        public static SomeTestClass Create()
+        {
+            return new SomeTestClass();
+        }
+
+        /// <summary>
+        /// Method that does some important things
+        /// </summary>
+        public static void SomeTestMethod()
+        {
+
+        }
+
+        /// <summary>
+        /// Calculate sum
+        /// </summary>
+        /// <param name="arr">Array that contains values</param>
+        /// <returns>The sum</returns>
+        public int MethodWithParams(params int[] arr)
+        {
+            int v = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                v += arr[i];
+            }
+            return v;
+        }
+    }
+}
