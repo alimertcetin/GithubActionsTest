93515f1f3cfc376ed443d2482f3ce43a462ffbaa
alimertcetin <mrtmert54@gmail.com>
2023-10-30 23:22:26 +0300

    feat: Add plain class

---
+++ TestApp/ClassWithNoCommentsOrAnything.cs
@@ -0,0 +1,19 @@
+namespace TestApp
+{
+    public class ClassWithNoCommentsOrAnything
+    {
+        public int intValue;
+        public int stringValue;
+        public AnotherImportantClass classValue;
+
+        public void AMethod()
+        {
+            
+        }
+
+        public int AFunction()
+        {
+            return -1;
+        }
+    }
+}
